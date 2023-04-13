7ca48a246aa0902ab1738ae290214718213f4f25
PrachayaKMITL <64011584@kmitl.ac.th>
2023-04-13 22:12:41 +0700

    test

--- NLPoperator.py
+++ NLPoperator.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class nlpCleaner :
     def extract_csv(df,title_column):
-        for i in len(df.rows):
+        for i in len(df.axes(0)):
             text_column = np.array(df[title_column]) 
 
 
